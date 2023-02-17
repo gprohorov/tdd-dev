e98120f8bab0772147816dd2f14993a9aa65b0a7
gprohorov <george.prokhorov@external.getneteurope.com>
2023-02-17 17:50:44 +0200

    11 tests

--- src/main/java/edu/pro/tdddev/TddDevApplication.java
+++ src/main/java/edu/pro/tdddev/TddDevApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TddDevApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(TddDevApplication.class, args);
+
     }
 
 }
